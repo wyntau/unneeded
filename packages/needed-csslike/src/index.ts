bf1a8cc3b2bf505915fa8ba3a8a1e653d41efde7
Wyntau <wyntau@outlook.com>
2019-07-06 17:19:49 +0800

    refactor: update needed-csslike options

--- packages/needed-csslike/src/index.ts
+++ packages/needed-csslike/src/index.ts
@@ -1,9 +1,7 @@
 import detectivePostcss from 'detective-postcss';
 import sass from 'sass';
 
-export type IFileType = 'css' | 'sass' | 'scss' | 'less' | 'stylus' | 'styl';
-
-export function neededCSSLike(fileContent: string, type: IFileType = 'css'): Array<string> {
+export function neededCSSLike(fileContent: string, type: string = 'css'): Array<string> {
   switch (type) {
     case 'css':
       return detectivePostcss(fileContent, { url: true });
